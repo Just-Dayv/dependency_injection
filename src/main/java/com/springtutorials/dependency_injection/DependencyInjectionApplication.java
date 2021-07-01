4c21340b899237abfa29e1fb1fd598971264ae4c
David <iyanuoluwaopa@gmail.com>
2021-07-01 16:57:28 +0100

    Utilised ApplicationContext to call MyController as a context bean

--- src/main/java/com/springtutorials/dependency_injection/DependencyInjectionApplication.java
+++ src/main/java/com/springtutorials/dependency_injection/DependencyInjectionApplication.java
@@ -1,13 +1,23 @@
 package com.springtutorials.dependency_injection;
 
+import com.springtutorials.dependency_injection.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DependencyInjectionApplication.class, args);
+
+        ApplicationContext context  = SpringApplication.run(DependencyInjectionApplication.class, args);
+
+        MyController myController = (MyController) context.getBean("myController");
+
+        String greeting = myController.sayHello();
+
+        System.out.println(greeting );
+
     }
 
 }
--- src/main/java/com/springtutorials/dependency_injection/controllers/MyController.java
+++ src/main/java/com/springtutorials/dependency_injection/controllers/MyController.java
@@ -1,4 +1,13 @@
 package com.springtutorials.dependency_injection.controllers;
 
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class   MyController {
+
+    public String sayHello(){
+        System.out.println("Hi World");
+        return "Hello World";
+    }
 }
